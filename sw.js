a43c8731b06b245a11a6a3447585f51570676f59
pavani17699A0537 <48669156+pavani17699A0537@users.noreply.github.com>
2019-06-26 17:45:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,24 +4,24 @@ self.addEventListener('install', function(event) {
     caches.open('cache1').then(function(cache) {
       return cache.addAll([
         './',
-			'./index.html',
-			'./restaurant.html',
-			'./serviceWorker.js',
-			'./js/dbhelper.js',
-			'./js/main.js',
-			'./js/restaurant_info.js',
-			'./css/styles.css',
-			'./data/restaurants.json',
-			'./img/1.jpg',
-			'./img/2.jpg',
-			'./img/3.jpg',
-			'./img/4.jpg',
-			'./img/5.jpg',
-			'./img/6.jpg',
-			'./img/7.jpg',
-			'./img/8.jpg',
-			'./img/9.jpg',
-			'./img/10.jpg'
+	'./index.html',
+	'./restaurant.html',
+	'./serviceWorker.js',
+	'./js/dbhelper.js',
+	'./js/main.js',
+	'./js/restaurant_info.js',
+	'./css/styles.css',
+	'./data/restaurants.json',
+	'./img/1.jpg',
+	'./img/2.jpg',
+	'./img/3.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg'
       ]);
     })
   );
